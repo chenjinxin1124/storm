3bf4304a61eadc2b053633f54200cce1b260f718
chenjinxin <chenjinxin@chenjinxin1124.cn>
2021-03-21 00:14:52 +0800

    滑动窗口，基于tuple数量

---
+++ src/main/java/start/window/sliding/Demo.java
@@ -0,0 +1,38 @@
+package start.window.sliding;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseWindowedBolt;
+import start.window.AmtSpout;
+
+public class Demo {
+    public static void main(String[] args) {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("spout", new AmtSpout());
+        builder.setBolt("slidingBolt",
+                new SlidingBolt()
+                        .withWindow(
+                                BaseWindowedBolt.Count.of(4),
+                                BaseWindowedBolt.Count.of(3)
+                        ),
+                1)
+                .shuffleGrouping("spout");
+
+        Config config = new Config();
+        config.setDebug(false);
+
+        try {
+            if (args != null && args.length > 0) {
+                config.setNumWorkers(3);
+                StormSubmitter.submitTopology(args[0], config, builder.createTopology());
+            } else {
+                LocalCluster cluster = new LocalCluster();
+                cluster.submitTopology("tumblingWindow", config, builder.createTopology());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/start/window/sliding/SlidingBolt.java
@@ -0,0 +1,31 @@
+package start.window.sliding;
+
+import org.apache.storm.topology.base.BaseWindowedBolt;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.windowing.TupleWindow;
+
+import java.util.List;
+
+public class SlidingBolt extends BaseWindowedBolt {
+    @Override
+    public void execute(TupleWindow inputWindow) {
+        int amt = 0;
+        List<Tuple> windowNew = inputWindow.getNew();
+        List<Tuple> windowExpired = inputWindow.getExpired();
+
+        System.out.println("windowNew");
+        for (Tuple tuple : windowNew) {
+            int a = Integer.parseInt(tuple.getStringByField("amt"));
+            System.out.println(tuple.getStringByField("id") + ": " + a);
+            amt += a;
+        }
+
+        System.out.println("windowExpired");
+        for (Tuple tuple : windowExpired) {
+            int a = Integer.parseInt(tuple.getStringByField("amt"));
+            System.out.println(tuple.getStringByField("id") + ": " + a);
+            amt -= a;
+        }
+        System.out.println("新增数量: "+amt);
+    }
+}
