78144e767ac205c69315cec10029ef883b05049f
chenjinxin <chenjinxin@chenjinxin1124.cn>
2021-03-28 17:20:02 +0800

    Trident Operations: project

---
+++ src/main/java/start/case6TridentTest/operations/project/Demo.java
@@ -0,0 +1,37 @@
+package start.case6TridentTest.operations.project;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.trident.TridentTopology;
+import org.apache.storm.trident.testing.FixedBatchSpout;
+import org.apache.storm.tuple.Fields;
+import start.case6TridentTest.operations.Datas;
+import start.case6TridentTest.operations.filter.Filter;
+import start.case6TridentTest.operations.function.Function;
+
+public class Demo {
+
+    public static StormTopology buildTopology() {
+
+        FixedBatchSpout spout = new Datas().getSpout();
+        spout.setCycle(true);
+
+        TridentTopology topology = new TridentTopology();
+        topology.newStream("spout", spout)
+                .each(new Fields("date", "amt", "city", "product"), new Function.MyFunction(), new Fields("_date"))// 此时的Tuple有"date", "amt", "city", "product", "_date"5列数据
+                .project(new Fields("_date", "amt"))// 投影操作：只保留 _date,amt 两个字段的数据，相当于select选取字段
+                .each(new Fields("amt", "_date"), new Filter.PrintFilter());// 打印 "amt", "_date"
+        return topology.build();
+    }
+
+    public static void main(String[] args) {
+        Config conf = new Config();
+        conf.setMaxSpoutPending(20);
+        conf.setDebug(false);
+        if (args.length == 0) {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("wordCounter", conf, buildTopology());
+        }
+    }
+}
