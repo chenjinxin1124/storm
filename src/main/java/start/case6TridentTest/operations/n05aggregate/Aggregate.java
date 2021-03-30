82bbae523940364a013786aaaf4645aeb5fbdf0a
chenjinxin <chenjinxin@chenjinxin1124.cn>
2021-03-30 20:53:34 +0800

    BaseAggregator

--- src/main/java/start/case6TridentTest/operations/n05aggregate/Aggregate.java
+++ src/main/java/start/case6TridentTest/operations/n05aggregate/Aggregate.java
@@ -1,8 +1,10 @@
 package start.case6TridentTest.operations.n05aggregate;
 
-import org.apache.storm.trident.operation.CombinerAggregator;
-import org.apache.storm.trident.operation.ReducerAggregator;
+import org.apache.storm.trident.operation.*;
 import org.apache.storm.trident.tuple.TridentTuple;
+import org.apache.storm.tuple.Values;
+
+import java.util.Map;
 
 public class Aggregate {
     public static class ReducerAggre implements ReducerAggregator {
@@ -19,7 +21,7 @@ public class Aggregate {
         }
     }
 
-    public static class CombinerAggre implements CombinerAggregator{
+    public static class CombinerAggre implements CombinerAggregator {
         @Override
         public Object init(TridentTuple tuple) {
             long amt = tuple.getIntegerByField("amt");
@@ -30,12 +32,53 @@ public class Aggregate {
 
         @Override
         public Object combine(Object val1, Object val2) {
-            return (long)val1 + (long)val2;
+            return (long) val1 + (long) val2;
         }
 
         @Override
         public Object zero() {
             return 0L;
         }
+    }
+
+    public static class BaseAgger extends BaseAggregator<BaseAgger.CountState> {
+        int parNum;
+        int parIndex;
+
+        static class CountState {
+            long count = 0;
+            long amtSum = 0;
+            Object _batchId;
+            String date;
+
+            CountState(Object _batchId) {
+                this._batchId = _batchId;
+            }
+        }
+
+        @Override
+        public void prepare(Map conf, TridentOperationContext context) {
+            parNum = context.numPartitions();
+            parIndex = context.getPartitionIndex();
+        }
+
+        @Override
+        public CountState init(Object batchId, TridentCollector collector) {
+            return new CountState(batchId);
+        }
+
+        @Override
+        public void aggregate(CountState val, TridentTuple tuple, TridentCollector collector) {
+            Integer amt = tuple.getIntegerByField("amt");
+            val.date = tuple.getStringByField("_date");
+            val.amtSum += amt;
+        }
+
+        @Override
+        public void complete(CountState val, TridentCollector collector) {
+            System.out.println("date = " + val.date + "\tparNum => " + parNum + "\tbatchId = " + val._batchId + "\tsum = " + val.amtSum);
+            collector.emit(new Values(val.amtSum));
+        }
+
     }
 }
